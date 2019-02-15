946ed1748ab3c4e034eb842022686a3c8b69962c
Frans100 <fheijne@gmail.com>
2019-02-15 12:51:55 +0100

    Een gemakkelijke manier om huizen toe te voegen aan de mysql database

---
+++ dbhuistoevoegen.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<body>
+    <form action="dbhuistoevoegen.php" method="POST">
+        <input name="steden" placeholder="Zoek op plaats">
+        <input name="huurprijs" type="number" placeholder="Huurprijs">
+        <input name="slaapkamers" type="number" placeholder="Aantal slaapkamers">
+        <input name="oppervlakte" type="number" placeholder="Oppervlakte">
+        <button type="submit">Voeg toe</button>
+    </form>   
+</body>
+<?php
+
+$steden = $_POST['steden'];
+$huurprijs = $_POST['huurprijs'];
+$slaapkamers = $_POST['slaapkamers'];
+$oppervlakte = $_POST['oppervlakte'];
+
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=dfss", 'root', '');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $statement = $conn->prepare('INSERT INTO huizen (steden, huurprijs, slaapkamers, oppervlakte) VALUES (:fsteden, :fhuurprijs, :fslaapkamers, :foppervlakte)');
+
+    $statement->execute([
+    'fsteden' => $steden,
+    'fhuurprijs' => $huurprijs,
+    'fslaapkamers' => $slaapkamers,
+    'foppervlakte' => $oppervlakte
+    ]);
+	
+}
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+	
+$conn = NULL;
+
+// header("Location: index.php");
+
+
+?>
+
+
+
+</html>
